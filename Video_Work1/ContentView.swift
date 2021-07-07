2c1a2775b0539ee35d514023ca10a8ebfffdce4c
Raza <rtshareef@gmail.com>
2021-07-07 16:41:20 -0400

    Initial Commit

---
+++ Video_Work1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Video_Work1
+//
+//  Created by Raza Shareef on 7/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
